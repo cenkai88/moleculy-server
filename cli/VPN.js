5d26eb46eb0fedc27c8a6b01d1351b11d2519e24
cenkai88 <cenkai29@qq.com>
2024-02-21 22:42:56 +0800

    init commit

--- cli/VPN.js
+++ cli/VPN.js
@@ -2,9 +2,12 @@ import inquirer from "inquirer";
 import { exec as execRaw } from "child_process";
 import util from "node:util";
 import chalk from "chalk";
+import ora from "ora";
 import { updateFile } from "./utils.js";
 
 const exec = util.promisify(execRaw);
+
+const spinner = ora();
 
 export default async () => {
   console.log("0 ->>>>>>>>>>>>> VPN初始化：\n");
@@ -54,9 +57,13 @@ export default async () => {
     filePath: "./wireguard/config/wg0.template",
     outputPath: "./wireguard/config/wg0.conf",
   });
-
+  
   // compose up
+  spinner.start("启动中...");
   const { stderr, stdout } = await exec(`sudo docker-compose up -d wireguard`);
   console.log(stderr, stdout);
-  console.log(chalk.red.bold("0 ->>>>>>>>>>>>> VPN启动完成"));
+  spinner.stopAndPersist({
+    symbol: chalk.green("✔"),
+    text: chalk.green.bold("0 ->>>>>>>>>>>>> VPN启动完成"),
+  });
 };
