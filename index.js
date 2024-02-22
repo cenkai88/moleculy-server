9c3d1c2a37be3aa11267eb39c0e7a275211be8db
cenkai88 <cenkai29@qq.com>
2024-02-22 17:13:02 +0800

    init commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 import figlet from "figlet";
 import chalk from "chalk";
+import hostile from "hostile";
 
 import vpnPrompt from "./cli/VPN.js";
 import minioPrompt from "./cli/minio.js";
@@ -39,6 +40,16 @@ console.table(components);
   },
 ]));
 
+hostile.set('127.0.0.1', 'minio-0.moleculy.com');
+hostile.set('127.0.0.1', 'web-0.moleculy.com');
+hostile.set('127.0.0.1', 'api-0.moleculy.com');
+hostile.set('127.0.0.1', 'application-0.moleculy.com');
+hostile.set('127.0.0.1', 'app-api-0.moleculy.com');
+hostile.set('127.0.0.1', 'ws-0.moleculy.com');
+hostile.set('127.0.0.1', 'cms-0.moleculy.com');
+hostile.set('127.0.0.1', 'thumbnail-0.moleculy.com');
+
+console.log('设置DNS完成');
 
 await vpnPrompt();
 components[0].status = "OK";
